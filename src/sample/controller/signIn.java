c747724903afd4e31c4eb078efda5b3152b0b091
ProMohamedReda <20812016100828@eng.zu.edu.eg>
2021-04-19 15:15:29 +0200

    sign in with db ready to work congratulation!

--- src/sample/controller/signIn.java
+++ src/sample/controller/signIn.java
@@ -3,14 +3,19 @@ package sample.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import java.sql.*;
+
+import javafx.scene.paint.Color;
 
 
 public class signIn implements Initializable {
@@ -21,12 +26,19 @@ public class signIn implements Initializable {
     private Connection dbconn;
     private Statement dbstm;
     private ResultSet dbres;
+    private ResultSet dbresLog;
+
 
     public String userdata;
+    @FXML
+    private TextField userName;
+    @FXML
+    private PasswordField password;
     @FXML
     private Button signButton;
     @FXML
     private Label loginText;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -34,11 +46,12 @@ public class signIn implements Initializable {
 
 
     public void buttonAlert(javafx.event.ActionEvent actionEvent) throws SQLException {
- //       loginText.setText("you enter the sign in button successfully");
-        dbconn = DriverManager.getConnection(dburl,dbuser,dbpass);
+
+        //       loginText.setText("you enter the sign in button successfully");
+        dbconn = DriverManager.getConnection(dburl, dbuser, dbpass);
         dbstm = dbconn.createStatement();
         dbres = dbstm.executeQuery("SELECT * from kunafahut.user");
-        while(dbres.next()) {
+        while (dbres.next()) {
             String username = dbres.getString(1);
             String phone = dbres.getString(2);
             String password = dbres.getString(3);
@@ -46,8 +59,35 @@ public class signIn implements Initializable {
             userdata += "username : " + username + " phone: " + phone + " password: " + password + " timedate: " + timedate + " .\n";
         }
         System.out.println(userdata);
-        loginText.setText(userdata);
-        //ok google
+        if (userName.getText().isEmpty() == true) {
+            loginText.setText("please enter the user name and password");
+        } else try {
+            try {
+                dbresLog = dbstm.executeQuery("SELECT * from kunafahut.user where username = '" + userName.getText() + "' AND password = '" + password.getText() + "'");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            String databaseUsername = "";
+            String databasePassword = "";
+            while (dbresLog.next()) {
+                databaseUsername = dbresLog.getString("username");
+                databasePassword = dbresLog.getString("password");
+            }
+            System.out.println(databaseUsername + "\n" + databasePassword + "\n" + password);
 
+
+            if (userName.getText().equals(databaseUsername) && password.getText().equals(databasePassword)) {
+                loginText.setText("Congratulation you had already signed in");
+                loginText.setTextFill(Color.GREEN);
+            } else {
+                loginText.setText("Please try again");
+                loginText.setTextFill(Color.RED);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            e.getCause();
+        }
     }
 }
